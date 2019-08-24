f4a93ccc9e44cde4893a9a56afbe135cde68b6d0
leolion0 <leo.c.brown1@gmail.com>
2019-08-23 20:27:55 -0500

    python to pythons file now works as expected

--- pythonser.py
+++ pythonser.py
@@ -1,14 +1,14 @@
-in_sent = input("Please input a sentence containing the word \"Python\": ")
-# in_sent = "hello python man"
+in_sent = input("Please input a sentence containing the word \"python\": ")
+
 new_out = ""
 ind = in_sent.find("python")
-while ind is not -1:
-    ind = in_sent.find("python ")
-    subs = in_sent[ind: ind + 6]
-    if ind is not -1:
-        temp = in_sent[:ind + 6] + "s" + in_sent[ind + 6:]
-        new_out = new_out + temp
-    old_ind = ind
-    ind = new_out[old_ind + 7:].find("python")
+counter = 1
 
-print(new_out)
+while ind is not -1:
+    if ind is not -1:
+        in_sent = in_sent[:ind] + "pythons " + in_sent[ind + 7:]
+    old_ind = ind
+    counter += 1
+    ind = in_sent.find("python ")
+
+print(in_sent)
